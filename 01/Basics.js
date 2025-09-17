5492682615ee669a4875e28dfeffbc33aa2662da
varun <varunkr9866@gmail.com>
2025-09-17 11:18:53 +0530

    STARTED WITH JS BASICS ON COMMENTS

--- 01/Basics.js
+++ 01/Basics.js
@@ -1,4 +1,14 @@
 //Printing Statments in JavaScript
 console.log("Hello Varun");
-console.log('My name is varun')
+console.log('My name is varun');
 console.log(2002);
+
+//Single Line Comment & Multiline Comment
+
+console.log("Varun"); //Single Line Commit
+
+/*
+console.log('My name is varun');
+console.log(1987);
+We are Writing anything over here is commented
+*/
