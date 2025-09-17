6709ee0f698803a3248811dfa21db917c698069e
varun <varunkr9866@gmail.com>
2025-09-17 12:43:49 +0530

    STARTED WITH JS NON-BOOLEAN INSIDE STATMENT AS PARAMETERS

--- 01/condition&functions.js
+++ 01/condition&functions.js
@@ -3,3 +3,10 @@ let flag = true;
 if(flag){
     console.log("Salute,salute,Salute");
 }
+
+//NON-BOOLRAN INSIDE IF CONDITION
+/*
+FOR NUM '0' IS false,REMAINING NUMBER ITS true;
+STRING '' , "" & NULL ALL ARE false;
+undefined OR NULL OR NaN IS false;
+*/
