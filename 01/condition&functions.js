d35f5bdd3554022b3ae1775f4eab8615e942f231
varun <varunkr9866@gmail.com>
2025-09-17 12:48:57 +0530

    STARTED WITH JS ALTERNATIVE VALUES

--- 01/condition&functions.js
+++ 01/condition&functions.js
@@ -10,3 +10,14 @@ FOR NUM '0' IS false,REMAINING NUMBER ITS true;
 STRING '' , "" & NULL ALL ARE false;
 undefined OR NULL OR NaN IS false;
 */
+
+//SHORT CIRCUIT ASSIGNMENT
+let myName = 'varun';
+let person = myName || 'nobody';
+console.log(`You know ,${person} is a good person`);
+
+let Name = '';
+let Person = Name || 'nobody';
+console.log(`You know ,${Person} is a good person`);
+
+
