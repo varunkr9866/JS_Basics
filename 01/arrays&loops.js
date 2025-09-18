7424a358502155eb4277e7b6090c2f45322ced2f
varun <varunkr9866@gmail.com>
2025-09-18 12:48:06 +0530

    WORKING ON ARRAYS ON push and pull

--- 01/arrays&loops.js
+++ 01/arrays&loops.js
@@ -8,3 +8,7 @@ let southAsia = ['India','Pakistan','Bangladesh','Buthan','Burma','Nepal'];
 southAsia[4] = 'Myannmar';
 console.log(`Burma is replaced by ${southAsia[4]} recently!`);
 
+//ARRAY PUSH  METHOD
+const northAmerica = ['Canada','United States of america','Mexico'];
+northAmerica.push('Panama');
+console.log(northAmerica[3]);
