4bf1f85112e5d38d9206c19d2a481c660557066d
varun <varunkr9866@gmail.com>
2025-09-18 12:44:55 +0530

    WORKING ON ARRAYS ON REPLACING

--- 01/arrays&loops.js
+++ 01/arrays&loops.js
@@ -1,3 +1,10 @@
 //ARRAYS
 let southEastAsia = ['India', 'Pakistan', 'Bangladesh'];
 console.log(southEastAsia);
+
+//ACCESSING ARRAYS
+let southAsia = ['India','Pakistan','Bangladesh','Buthan','Burma','Nepal'];
+
+southAsia[4] = 'Myannmar';
+console.log(`Burma is replaced by ${southAsia[4]} recently!`);
+
