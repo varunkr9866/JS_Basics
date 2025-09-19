6cca017bd4d3806dccc474f77c0391d274247939
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-09-19 15:21:33 +0530

    ACCESSING THE VALUES BY USING KEYS IN JS OBJECTS'

--- objects_in_js.js
+++ objects_in_js.js
@@ -10,3 +10,7 @@ let dog = {
 let dogColor = dog.color;
 console.log(`color of the Dog is : ${dogColor}`);
 console.log(`Weight of the dog is : ${dog.weight} Kilos`);
+
+let response = dog ['response to'];
+console.log(`My dog response to ${response}`);
+console.log(dog.tag_no);
