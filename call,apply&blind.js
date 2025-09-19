782eee55ee8c13104fe51e3af65a554f217762b6
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-09-19 17:08:12 +0530

    CALL METHOD OTHER WAY

--- call,apply&blind.js
+++ call,apply&blind.js
@@ -18,6 +18,7 @@ personOne.printName();
 personTwo.printName();
 
 //FUNCTION BORROWING 
+//APPROACH METHOD 1
 let personOne = {
     firstName: 'Varun',
     lastName: 'Reddy',
@@ -30,3 +31,18 @@ let personTwo = {
     lastName: 'tendulkar',
 }
 personOne.printName.call(personTwo);
+
+//FUNCTION BORROWING 
+//APPROACH METHOD 2
+let personName = function (obj){
+        console.log(this.firstName + " " + this.lastName);
+    }
+let personOne = {
+    firstName: 'Varun',
+    lastName: 'Reddy',
+}
+let personTwo = {
+    firstName: 'sachin',
+    lastName: 'tendulkar',
+}
+printName(personTwo);
