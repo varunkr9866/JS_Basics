e95fb8aa479eaa2a2be79461382bfe9c2e84bce1
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-09-19 17:16:33 +0530

    BIND METHOD

--- call,apply&blind.js
+++ call,apply&blind.js
@@ -60,4 +60,24 @@ let personTwo = {
     firstName: 'sachin',
     lastName: 'tendulkar',
 }
-printName(personTwo);
+printName.call(personOne, 'kadapa', 'AP');
+printName.apply(personOne, ['kadapa', 'AP']);
+
+//FUNCTION BORROWING 
+//BIND METHOD
+let printName = function (hometown, state) {
+    console.log(`\n ${this.firstName} ${this.lastName} form ${hometown},${state}`);
+}
+let personOne = {
+    firstName: 'Varun',
+    lastName: 'Reddy',
+}
+let personTwo = {
+    firstName: 'sachin',
+    lastName: 'tendulkar',
+}
+let f1 = printName.bind(personOne, 'kadapa', 'AP');
+let f2 = printName.bind(personOne, ['kadapa']);
+
+f1();
+f2();
