0cee895099df80f894df0611ee9ad681c61b62f6
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-09-19 15:12:47 +0530

    OBJECT IN JAVASCRIPT

---
+++ objects_in_js.js
@@ -0,0 +1,12 @@
+//OBJECTS IN JAVASCRIPT
+let dog = {
+    breed : 'Golden Retriver',
+    color : 'Orange',
+    weight : 18,
+    'response to' : ['wistle','clap','throw ball'],
+    tag_no : 776
+}
+
+let dogColor = dog.color;
+console.log(`color of the Dog is : ${dogColor}`);
+console.log(`Weight of the dog is : ${dog.weight} Kilos`);
