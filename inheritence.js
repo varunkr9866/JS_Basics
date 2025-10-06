052f95be99f7d27363e1ea6712e48ba654074e9a
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-06 15:13:47 +0530

    COMPLETED EXTEND KEYWORD IN JS

--- inheritence.js
+++ inheritence.js
@@ -9,3 +9,16 @@ cosole.log("value of x : +this.x");
 cosole.log("value of y : +this.y");
 }
 }
+class Beta extends Alpha{
+constructor(x,y,z){
+super(x,y);
+this.z = z;
+}
+showAlpha(){
+cosole.log("value of x : +this.x");
+cosole.log("value of y : +this.y");
+cosole.log("value of z : +this.z");
+}
+sum(){
+return() this.x+this.y+this.z;
+}
