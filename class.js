5c19a5b700504053c3981585fd918376e2c1c4e8
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-06 14:50:16 +0530

    COMPLETED WITH CLASS REPRESENTATION IN JS

--- class.js
+++ class.js
@@ -25,3 +25,35 @@ let d1 = dogFactory('Labrador',18,'Orange');
 
 //class representation in js
 
+class Dog{
+constructor(breed,weight,color){
+this._breed:breed,
+this._weight:weight,
+this._color:color,
+
+//Getter&setter methods
+get breed(){
+return this_breed;
+}
+get weight(){
+return this_weight;
+}
+
+get color(){
+return this_color;
+}
+
+set breed(newBreed){
+if(typeof newBreed == 'string'){
+this._breed = newBreed;
+}
+set weight(newWeight){
+if(typeof newWeight == 'string'){
+this._weight = newWeight;
+}
+set color(newColor){
+if(typeof newColor == 'string'){
+this._color = newColor;
+}
+}
+}
