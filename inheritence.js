97eb6fb21d8ecaabecfbc2c8f27d66a180a11b03
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-06 15:18:30 +0530

    COMPLETED STATIC METHODS IN JS

--- inheritence.js
+++ inheritence.js
@@ -8,8 +8,15 @@ showAlpha(){
 cosole.log("value of x : +this.x");
 cosole.log("value of y : +this.y");
 }
+static sayHello(){
+cosole.log("Hello everyone!!");
+}
+
 }
 class Beta extends Alpha{
+static sayName(person){
+cosole.log("Namaste ${person}, welcome to india!");
+}
 constructor(x,y,z){
 super(x,y);
 this.z = z;
@@ -22,3 +29,5 @@ cosole.log("value of z : +this.z");
 sum(){
 return() this.x+this.y+this.z;
 }
+
+//TO ACCESS THE STATIC METHOD IN JS
