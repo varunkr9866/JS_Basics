96722034ac0a30df8adc534240389c43475dc268
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-22 14:01:35 +0530

    STARTED BASICS WITH CONSTRUCTORS IN JS

---
+++ Constructors.js
@@ -0,0 +1,18 @@
+class Box{
+    constructor(l,b,h){
+        this.l = l;
+        this.b = b;
+        this.h = h;
+    }
+    printVolume(){
+        console.log(`Volume of the box is : ${this.l * this.b * this.h}`);
+    }
+    fetchSurfaceArea(){
+        return 2*(this.l * this.b + this.b * this.h + this.h * this.b)
+    }
+    setBoxSize(){
+         this.l = l;
+        this.b = b;
+        this.h = h;
+    }
+}
