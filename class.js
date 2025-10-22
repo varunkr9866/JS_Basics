0d1f4c43384f157bc16d0e4716da8a5258c7f61b
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-22 13:43:23 +0530

    STARTED WORKING WITH CONSTRUCTORS IN JS

--- class.js
+++ class.js
@@ -21,48 +21,48 @@ const dogFactory = (breed,weight,color) =>{
  }
 }
 
-let d1 = dogFactory('Labrador',18,'Orange');
+// let d1 = dogFactory('Labrador',18,'Orange');
 
-//class representation in js
+// //class representation in js
 
-class Dog{
-constructor(breed,weight,color){
-this._breed:breed,
-this._weight:weight,
-this._color:color,
+// class Dog{
+// constructor(breed,weight,color){
+// this._breed:breed,
+// this._weight:weight,
+// this._color:color,
 
 //Getter&setter methods
-get breed(){
-return this_breed;
-}
-get weight(){
-return this_weight;
-}
+// get breed(){
+// return this_breed;
+// }
+// get weight(){
+// return this_weight;
+// }
 
-get color(){
-return this_color;
-}
+// get color(){
+// return this_color;
+// }
 
-set breed(newBreed){
-if(typeof newBreed == 'string'){
-this._breed = newBreed;
-}
-set weight(newWeight){
-if(typeof newWeight == 'string'){
-this._weight = newWeight;
-}
-set color(newColor){
-if(typeof newColor == 'string'){
-this._color = newColor;
-}
-else{
-console.log("Please enter correct values")
-}
-}
-}
+// set breed(newBreed){
+// if(typeof newBreed == 'string'){
+// this._breed = newBreed;
+// }
+// set weight(newWeight){
+// if(typeof newWeight == 'string'){
+// this._weight = newWeight;
+// }
+// set color(newColor){
+// if(typeof newColor == 'string'){
+// this._color = newColor;
+// }
+// else{
+// console.log("Please enter correct values")
+// }
+// }
+// }
 
-//this represents object level variables
+// //this represents object level variables
 
-//dereferencing the objects
-var object1 = new Object();
-object1 = null;
+// //dereferencing the objects
+// var object1 = new Object();
+// object1 = null;
