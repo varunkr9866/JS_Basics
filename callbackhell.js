d538531f47aa0f0dd8c1dcbcd6fec12743beb3bd
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-22 19:05:05 +0530

    STARTED WITH CALLBACK HELL IN JS

---
+++ callbackhell.js
@@ -0,0 +1,13 @@
+let balajiBath = function(){
+    console.log(`\n Balaji Woke Up`)
+    console.log(`\n Balaji is Bathing`)
+    console.log(`\n Balaji is Ready`)
+}
+let kishanBath = function(){
+    console.log(`\n kishan Woke Up`)
+    console.log(`\n kishan is Bathing`)
+    console.log(`\n kishan is Ready`)
+}
+
+balajiBath();
+kishanBath();
