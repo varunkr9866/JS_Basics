6672db92d0095a82a3cbfcdcad3dd3800900a230
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-23 13:18:17 +0530

    STARTED HTML CODE WITH ASYNC & AWAIT IN JS

--- Async&Await.js
+++
@@ -1,5 +0,0 @@
-function balaji(){
-    return 'balaji'
-}
-let b = balaji();
-console.log(b);
---
+++ Async&Await/Async&Await.js
@@ -0,0 +1,12 @@
+// function balaji(){
+//     return 'balaji'
+// }
+// let b = balaji();
+// console.log(b);
+
+
+async function balaji(){
+    return 'balaji'
+}
+let b = balaji();
+console.log(b);
