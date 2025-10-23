70e367248cc5bc593983bc763645ceacf4286025
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-23 11:25:12 +0530

    COMPLETED THE OTHER WAYS OF WORKING ON PROMISES IN JS

--- Promises.js
+++ Promises.js
@@ -1,22 +1,21 @@
-const bringChoclates = function (){
-    let didBringChoclates = false;
-    console.log('Getting Ready to go to markets');
-    console.log('Booking a cab to the store');
-    console.log('Selecting the Choclates');
-    console.log('Making the payments');
-    
-    didIBringChoclates = true;
+// const bringChoclates = function (){
+//     let didBringChoclates = false;
+//     console.log('Getting Ready to go to markets');
+//     console.log('Booking a cab to the store');
+//     console.log('Selecting the Choclates');
+//     console.log('Making the payments');
 
-    if (didIBringChoclates == true) {
-        console.log('kishan says: Thanks You appa!');
-        
-    } else {
-        console.log('Kishan Crying in the Corner')
-    }
-}
+//     didIBringChoclates = true;
 
-bringChoclates();
+//     if (didIBringChoclates == true) {
+//         console.log('kishan says: Thanks You appa!');
 
+//     } else {
+//         console.log('Kishan Crying in the Corner')
+//     }
+// }
+
+// bringChoclates();
 
 //  THE OTHER WAY TO DO IT
 // const bringChoclates = function (){
@@ -25,15 +24,46 @@ bringChoclates();
 //     setTimeout(() =>{ console.log('Booking a cab to the store')},4000);
 //     setTimeout(() =>{ console.log('Selecting the Choclates')},6000);
 //     setTimeout(() =>{ console.log('Making the payments')},8000);
-//     setTimeout(() =>{didIBringChoclates = true});
+
+//     setTimeout(() =>{
+// didIBringChoclates = true;
 
 //     if (didIBringChoclates == true) {
 //         console.log('kishan says: Thanks You appa!');
-        
+
 //     } else {
 //         console.log('Kishan Crying in the Corner')
 //     }
+// },12000);
 // }
 
 // bringChoclates();
 
+//THE OTHER WAY TO DO IT
+const didIBringChoclates = function (resolve, reject) {
+  let didIBringChoclates = false;
+  setTimeout(() => {
+    console.log("Getting Ready to go to markets");
+  }, 2000);
+  setTimeout(() => {
+    console.log("Booking a cab to the store");
+  }, 4000);
+  setTimeout(() => {
+    console.log("Selecting the Choclates");
+  }, 6000);
+  setTimeout(() => {
+    console.log("Making the payments");
+  }, 8000);
+
+  setTimeout(() => {
+    didIBringChoclates = true;
+
+    if (didIBringChoclates == true) {
+      resolve();
+    } else {
+      reject();
+    }
+  }, 12000);
+};
+
+bringChoclates();
